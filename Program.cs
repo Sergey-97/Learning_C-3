efac7c16a44d3355a7211fd1e4bf21e9810f0acb
Sergey <aseev-1977@list.ru>
2022-12-23 18:04:13 +0300

    Задача 6

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+            Console.WriteLine("Введите число:");
+            number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 1)
+            {
+                Console.WriteLine("Число " + number + " является: нечётным");
+            }
+            else
+            {
+                Console.WriteLine("Число " + number + "является: чётным");
+            }
